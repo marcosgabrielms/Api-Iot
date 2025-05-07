9c9d9d747ebf008668b5f2b6ad8a9a0caba91b2a
marcosgabrielms <marcosgabriel166@gmail.com>
2025-05-07 08:48:45 -0300

    :truck: chore: modifica função name

--- app.py
+++ app.py
@@ -75,6 +75,6 @@ def dashboard_joystick():
     return render_template_string(html, x=x, y=y, direcao=direcao)
 
 if __name__ == '__main__':
-    port = int(os.enviroment.get("PORT, 10000"))
+    port = int(os.environ.get("PORT, 10000"))
     app.run(host= '0.0.0.0', port=port)
 
